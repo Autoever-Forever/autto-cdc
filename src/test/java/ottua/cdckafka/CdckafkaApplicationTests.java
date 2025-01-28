e0932a21296a597b5b6a6932d5d8b8a47bfc1eb8
roomie00 <roomie@shin-ahreum.local>
2025-01-28 11:52:49 +0900

    프로젝트 초기 설정

---
+++ src/test/java/ottua/cdckafka/CdckafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ottua.cdckafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdckafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
